023e59c42f6d7fa97dc6b082d92cf8078f7eeb3c
Kavitha Nair <kavitha.nair09@gmail.com>
2017-08-08 17:37:27 +0530

    updated followers app

--- TwitterFollowersInsight/js/script.js
+++ TwitterFollowersInsight/js/script.js
@@ -1,8 +1,11 @@
 var app = angular.module('TweetSearch', []);
 app.controller('Controller', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
 
-    $scope.Search = function() {
+    $scope.Search = function(query) {
         $scope.spinner = true;
+        if (query !== undefined)
+            $scope.query = query;
+
         var QueryCommand = 'http://api.loklak.org/api/user.json?callback=JSON_CALLBACK&screen_name=' + $scope.query +
             '&followers=1000&following=1000';
 
@@ -18,22 +21,22 @@ app.controller('Controller', ['$scope', '$http', '$sce', function($scope, $http,
                 user = followers[i].screen_name;
                 name = followers[i].name;
                 followers_count = followers[i].followers_count;
-                following_count = followers[i].friends_count;
                 pic = followers[i].profile_image_url;
-                followerslist.push([user, pic, name, followers_count, following_count]);
+                followerslist.push([user, pic, name, followers_count]);
             }
 
             for (var i = 0; i < following.length; i++) {
                 user = following[i].screen_name;
                 name = following[i].name;
                 followers_count = following[i].followers_count;
-                following_count = followers[i].friends_count;
                 pic = following[i].profile_image_url;
-                followinglist.push([user, pic, name, followers_count, following_count]);
+                followinglist.push([user, pic, name, followers_count]);
             }
 
             $scope.followersStatus = followerslist;
             $scope.followingStatus = followinglist;
+            $scope.followersCount = data.user.followers_count;
+            $scope.followingCount = data.user.friends_count;
         });
     }
     $scope.spinner = false;
